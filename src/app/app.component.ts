4485aa51a2222d0a8d8f0c43b0a1016a5a8fdc07
esraa-hassan-ahmed <esraahassan152018@gmail.com>
2018-06-05 11:07:18 +0200

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,19 @@ export class AppComponent {
   redirectToRegister():void{
     this.router.navigate(['/register/']);
   }
-  
+
+//   window.onscroll = function() {scrollFunction()};
+//   function scrollFunction() {
+//       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//           document.getElementById("myBtn").style.display = "block";
+//       } else {
+//           document.getElementById("myBtn").style.display = "none";
+//       }
+//   }
+//   function topFunction() {
+//       document.body.scrollTop = 0;
+//       document.documentElement.scrollTop = 0;
+//   }
 
 }
+
